7bbabd0cbcfd8466d602b255ebfc3a14a6e85d80
Tedokon Ryota <s19014@std.it-college.ac.jp>
2019-05-24 14:02:44 +0900

    5/23課題修正

--- 0523/052305.sh
+++ 0523/052305.sh
@@ -1,4 +1,3 @@
 #find /etc/ -type f 2> /dev/null |xargs grep -L '172.16.40.1'
-find /etc/ -type f -print0 2> /dev/null |xargs -0 grep -L '172.16.40.1'
+find /etc/ -type f -print0 2> /dev/null |xargs -0 grep -l '172.16.40.1' 2> /dev/null
 #grep -rL '172.16.40.1' /etc 2> /dev/null
-
