f9a19d5b462a4ea474383eddd4f5a1be9139ea24
Tedokon Ryota <s19014@std.it-college.ac.jp>
2019-06-06 11:36:03 +0900

    6/4課題

---
+++ 0604/060402.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for file in "$@"
+do
+    if [ -f "$file" ]; then
+        # duでファイル容量を求める
+        du "$file"
+    else
+        # エラーメッセージ
+        echo "${file}:通常のファイルではありません"
+    fi
+done
---
+++ 0604/060403.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -d "$1" ]; then
+    # ディレクトリ内のファイルが実行可能ファイルかチェックして
+    # 実行可能ファイルであれば表示
+    #for file in $(ls "$1")
+    for file in $(find "$1" -maxdepth 1 -type f)
+    do
+        #fileが実行ファイルであれば表示
+        #if [ -x "${1}/${file}" ] && [ ! -d "${1}/${file}" ]; then
+        if [ -x "$file" ]; then
+            #echo "$file"
+            #echo `basename "$file"`
+            echo $(basename "$file")
+            #sudo ls -l "${file}"
+        fi
+    done
+else
+    # エラーメッセージ
+    echo "${1}: ディレクトリではありません"
+fi
